756ab19138525d831dff597afff7265c4cbfbb01
Juliette Cordor <grassojames5@gmail.com>
2022-06-28 11:22:16 +1000

    added demo image

--- tests/web.rs
+++ tests/web.rs
@@ -5,9 +5,18 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
+use avgcol::*;
+
 wasm_bindgen_test_configure!(run_in_browser);
 
+const DEMO_IMAGE: &[u8] = include_bytes!("image.jpg");
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+fn check_image() {
+    let image = AverageColor::from_bytes(DEMO_IMAGE);
+
+    let image = match image {
+        Ok(v) => v,
+        Err(_) => panic!("Failed to load image"),
+    };
 }

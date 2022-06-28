4d870ef1b593137d764853f17c009077ac9882c9
Juliette Cordor <grassojames5@gmail.com>
2022-06-28 11:34:47 +1000

    fixed issues with test

--- tests/web.rs
+++ tests/web.rs
@@ -14,7 +14,7 @@ wasm_bindgen_test_configure!(run_in_browser);
 const DEMO_IMAGE: &[u8] = include_bytes!("image.jpg");
 
 #[wasm_bindgen_test]
-fn check_image() {
+fn test_image_average_color() {
     let image = AverageColor::from_bytes(DEMO_IMAGE);
 
     let image = match image {
@@ -22,7 +22,7 @@ fn check_image() {
         Err(_) => panic!("Failed to load image"),
     };
 
-    let expected = AverageColor(178, 181, 172);
+    let expected = AverageColor(178, 180, 172);
 
     assert_eq!(image, expected);
 }

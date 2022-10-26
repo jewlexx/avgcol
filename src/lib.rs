aed2150152cdd6346327efc51581743018a415ce
Juliette Cordor <grassojames5@gmail.com>
2022-10-26 16:12:15 +1100

    updated tests and readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,16 @@
-mod utils;
-
-use wasm_bindgen::prelude::*;
-
 use image::RgbImage;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("Image error: {0}")]
+    ImageError(#[from] image::ImageError),
 
-macro_rules! sum {
-    ($vec:expr, $sum_type:tt) => {{
-        let mut sum: $sum_type = 0 as $sum_type;
-
-        for i in $vec {
-            sum += i as $sum_type;
-        }
-
-        sum
-    }};
+    #[error("Base64 error: {0}")]
+    Base64(#[from] base64::DecodeError),
 }
 
-#[wasm_bindgen]
+pub type Result<T> = std::result::Result<T, Error>;
+
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct AverageColor(pub u64, pub u64, pub u64);
 
@@ -34,36 +22,37 @@ impl Drop for AverageColor {
     }
 }
 
-#[wasm_bindgen]
 impl AverageColor {
-    fn from_mem<'a>(bytes: impl Into<&'a [u8]>) -> Result<RgbImage, JsError> {
+    fn from_mem<'a>(bytes: impl Into<&'a [u8]>) -> Result<RgbImage> {
         let bytes: &[u8] = bytes.into();
         let image_data = image::load_from_memory(bytes)?;
 
         Ok(image_data.to_rgb8())
     }
 
-    fn get_average_color(image_data: RgbImage) -> Result<AverageColor, JsError> {
+    fn get_average_color(image_data: RgbImage) -> Result<AverageColor> {
         let pixels: Vec<_> = image_data.pixels().cloned().collect();
         let pixels_length = pixels.len() as u64;
 
-        let red = sum!(pixels.iter().map(|pixel| pixel[0]), u64) / pixels_length;
-        let green = sum!(pixels.iter().map(|pixel| pixel[1]), u64) / pixels_length;
-        let blue = sum!(pixels.iter().map(|pixel| pixel[2]), u64) / pixels_length;
+        let red: u64 = pixels.iter().map(|pixel| u64::from(pixel[0])).sum();
+        let green: u64 = pixels.iter().map(|pixel| u64::from(pixel[1])).sum();
+        let blue: u64 = pixels.iter().map(|pixel| u64::from(pixel[2])).sum();
 
-        Ok(AverageColor(red, green, blue))
+        Ok(AverageColor(
+            red / pixels_length,
+            green / pixels_length,
+            blue / pixels_length,
+        ))
     }
 
-    #[wasm_bindgen(js_name = fromBytes)]
-    pub fn from_bytes(image_bytes: &[u8]) -> Result<AverageColor, JsError> {
+    pub fn from_bytes(image_bytes: &[u8]) -> Result<AverageColor> {
         let image_data = Self::from_mem(image_bytes)?;
 
         Self::get_average_color(image_data)
     }
 
-    #[wasm_bindgen(js_name = fromUrl)]
     #[cfg(feature = "remote_image")]
-    pub async fn from_url(url: String) -> Result<AverageColor, JsError> {
+    pub async fn from_url(url: String) -> Result<AverageColor> {
         let image = reqwest::get(url).await?;
         let bytes: &[u8] = &image.bytes().await?;
 
@@ -72,15 +61,13 @@ impl AverageColor {
         Self::get_average_color(image_data)
     }
 
-    #[wasm_bindgen(js_name = fromBase64)]
-    pub fn from_base64(base64: String) -> Result<AverageColor, JsError> {
+    pub fn from_base64(base64: String) -> Result<AverageColor> {
         let image_bytes = base64::decode(base64)?;
         let image_data = Self::from_mem(image_bytes.as_slice())?;
 
         Self::get_average_color(image_data)
     }
 
-    #[wasm_bindgen(js_name = isLight)]
     pub fn is_light(&self) -> bool {
         let red = self.0 as f64;
         let green = self.1 as f64;
@@ -89,5 +76,26 @@ impl AverageColor {
         let brightness = (red * 0.2126 + green * 0.7152 + blue * 0.0722) / 255.0;
 
         brightness > 0.5
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AverageColor;
+
+    const DEMO_IMAGE: &[u8] = include_bytes!("../tests/image.jpg");
+
+    #[test]
+    fn test_image_average_color() {
+        let image = AverageColor::from_bytes(DEMO_IMAGE);
+
+        let image = match image {
+            Ok(v) => v,
+            Err(_) => panic!("Failed to load image"),
+        };
+
+        let expected = AverageColor(178, 180, 172);
+
+        assert_eq!(image, expected);
     }
 }

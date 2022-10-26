36eced07990f6367ba06bab10af6331206f3664a
Juliette Cordor <grassojames5@gmail.com>
2022-10-26 15:41:56 +1100

    fixes

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,19 +1,11 @@
 //! Test suite for the Web and headless browsers.
-
-#![cfg(target_arch = "wasm32")]
-
-extern crate wasm_bindgen_test;
 use std::assert_eq;
-
-use wasm_bindgen_test::*;
 
 use avgcol::*;
 
-wasm_bindgen_test_configure!(run_in_browser);
-
 const DEMO_IMAGE: &[u8] = include_bytes!("image.jpg");
 
-#[wasm_bindgen_test]
+#[test]
 fn test_image_average_color() {
     let image = AverageColor::from_bytes(DEMO_IMAGE);
 
